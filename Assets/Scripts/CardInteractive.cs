3713b8a4445769ea1e9a9f966ffd39820296560f
彥廷 劉 <tim120946@gmail.com>
2025-01-21 15:02:54 +0800

    *add solider data display *Update solider card and venue interactive *add CardInteractive script annotation

--- Assets/Scripts/CardInteractive.cs
+++ Assets/Scripts/CardInteractive.cs
@@ -7,12 +7,12 @@ using UnityEngine.UI;
 public class CardInteractive : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     ,IBeginDragHandler, IDragHandler, IEndDragHandler
 { 
-    public GameObject cardInfo;
-    public GameObject solider;
+    public GameObject cardInfo;             //卡片詳細
+    public GameObject solider;              //士兵prefeb
 
-    public Vector2 origPos;
-    public Transform originParent;
-    private RectTransform currentTrans;
+    private Vector2 origPos;                //原位置
+    private Transform originParent;         //父物件
+    private RectTransform currentTrans;     //UI transform
     private CanvasGroup canvasGroup;
 
     public void Base_Start()
@@ -21,19 +21,26 @@ public class CardInteractive : MonoBehaviour, IPointerEnterHandler, IPointerExit
         currentTrans = GetComponent<RectTransform>();
         
     }
+    #region 滑鼠進出事件
 
+    //滑鼠碰到時，凸顯卡片位置並顯示卡片細節
     public void OnPointerEnter(PointerEventData eventData)
     {
         transform.position += new Vector3(0, 50f, 0);
         info = Instantiate(cardInfo, new Vector3(960f, 700f, 0f), 
             Quaternion.identity,GameObject.FindGameObjectWithTag("Canvas").transform);
     }
+    //滑鼠離開時，恢復原狀
     public void OnPointerExit(PointerEventData eventData)
     {
         transform.position -= new Vector3(0, 50f, 0);
         Destroy(info);
     }
     GameObject info;
+
+    #endregion
+
+    #region 滑鼠拖曳事件
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -45,7 +52,7 @@ public class CardInteractive : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public void OnDrag(PointerEventData eventData)
     {
         currentTrans.position = eventData.position;
-        Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
+        //Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
     }
     public void OnEndDrag(PointerEventData eventData)
     {
@@ -53,26 +60,38 @@ public class CardInteractive : MonoBehaviour, IPointerEnterHandler, IPointerExit
         transform.SetParent(originParent);
         currentTrans.position = origPos;
 
+        #region 和場地的互動
+
         if (eventData.pointerCurrentRaycast.gameObject.CompareTag("Scene"))
         {
             canvasGroup.blocksRaycasts = true;
-            //如果是士兵卡，生成角色到該位置;如果是場地卡，改變位置的場地
+            //如果是士兵卡，生成角色到該位置;如果是場地卡，改變該位置的場地
             if (gameObject.tag == "SoliderCard")
             {
-                Instantiate(solider, eventData.pointerCurrentRaycast.gameObject
+                GameObject soliderObj;
+                soliderObj = Instantiate(solider, eventData.pointerCurrentRaycast.gameObject
                     .transform.position, Quaternion.identity, eventData.pointerCurrentRaycast
                     .gameObject.transform);
+                //設置角色初始數據
+                soliderObj.GetComponent<SoliderActive>().txtData.text = gameObject.GetComponent<SoliderCard>().
+                    soliderCardData.attack.ToString() + " / " +
+                gameObject.GetComponent<SoliderCard>().soliderCardData.health.ToString();
+
+                soliderObj.GetComponent<SoliderActive>().txtSpd.text =
+                    gameObject.GetComponent<SoliderCard>().soliderCardData.move.ToString();
+
+                Destroy(gameObject);
             }
             else if (gameObject.tag == "VenueCard")
             {
                 eventData.pointerCurrentRaycast.gameObject.GetComponent<Image>().sprite
                     = gameObject.GetComponent<VenueCard>().venueCardData.cardSprite;
             }
-
             //transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform);
             //currentTrans.position = eventData.pointerCurrentRaycast
-                //.gameObject.transform.position;
+            //.gameObject.transform.position;
         }
+        #endregion
     }
-
+    #endregion
 }
---
+++ Assets/Scripts/SoliderActive.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SoliderActive : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    SoliderCardData soliderCardData;
+    GameObject cardInfo;
+    [SerializeField] SoliderCard soliderCard;
+
+    public Text txtData;                        //士兵攻防文字
+    public Text txtSpd;                         //士兵移動力文字
+
+    [System.NonSerialized]public int hp;        //士兵生命
+    [System.NonSerialized] public int atk;      //士兵攻擊
+    [System.NonSerialized] public int spd;      //士兵移動力
+    public Slider slidCounter;                  //回合計數器
+    private bool canMove;                       //是否能移動
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soliderCardData = soliderCard.soliderCardData;
+        cardInfo = soliderCard.cardInfo;
+        hp = soliderCardData.health;
+        atk = soliderCardData.attack;
+        spd = soliderCardData.move;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    #region 移動相關判定
+        //如果移動力有變動，顯示目前移動力
+        if (spd != soliderCardData.move)
+        {
+            txtSpd.transform.parent.gameObject.SetActive(true);
+        }
+        else
+        {
+            txtSpd.transform.parent.gameObject.SetActive(false);
+        }
+        //如果移動力歸0，無法移動並顯示計數器
+        if (spd <= 0 && slidCounter.value < 2)
+        {
+            slidCounter.gameObject.SetActive(true);
+            canMove = false;
+        }
+        else
+        {
+            slidCounter.gameObject.SetActive(false);
+            canMove = true;
+        }
+    #endregion
+
+        /*
+        txtData.text = soliderCard.soliderCardData.attack.ToString() + " / " +
+            soliderCard.soliderCardData.health.ToString();
+        txtSpd.text = soliderCard.soliderCardData.move.ToString();
+        */
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        info = Instantiate(cardInfo, new Vector3(960f, 700f, 0f),
+            Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Destroy(info);
+    }
+    GameObject info;
+}
