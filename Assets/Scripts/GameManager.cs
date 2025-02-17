daf9e81be9ef1079859265ec11210de8f1827e73
dragon120946 <tim120946@gmail.com>
2025-02-17 14:39:08 +0800

    *Add Day and night system *Add Turn End text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,8 +11,12 @@ public class GameManager : MonoBehaviour
     public bool isMyTurn;
     public bool isEnemyTurn;
     private bool alreadyTriggered = false;
+    private int n;
+    private int x;
     public Button btnTurnEnd;
     public Slider slidAp;
+    public Scrollbar scollDayAndNight;
+    public Text txtDay;
 
     // Start is called before the first frame update
     void Start()
@@ -26,9 +30,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        n = Mathf.RoundToInt(turn / 2);
+        x = Mathf.RoundToInt(n / 2);
+
         //若回合數為奇數，則為我方回合；若回合數為偶數，則為敵方回合
-        //Debug.Log("turn:" + turn);
-        int n = Mathf.RoundToInt(turn / 2);
+        Debug.Log("turn:" + turn);
         //Debug.Log("n:" + n);
         if (turn == n * 2 + 1)
         {
@@ -40,10 +46,53 @@ public class GameManager : MonoBehaviour
             isEnemyTurn = true;
             isMyTurn = false;
         }
+
         //我方回合才可行動
         if (isMyTurn)
         {
+            btnTurnEnd.interactable = true;
             TurnStart();
+        }
+        //敵方回合時，我方無法結束回合
+        if (isEnemyTurn)
+        {
+            btnTurnEnd.interactable = false;
+        }
+
+        #region 晝夜判定
+        if (n == 0 || n == x * 2 && isMyTurn)
+        {
+            scollDayAndNight.value = 0f;
+        }
+        else if (n == x * 2 && isEnemyTurn)
+        {
+            scollDayAndNight.value = 0.33f;
+        }
+        else if (n == x * 2 + 1 && isMyTurn)
+        {
+            scollDayAndNight.value = 0.66f;
+        }
+        else if (n == x * 2 + 1 && isEnemyTurn)
+        {
+            scollDayAndNight.value = 1f;
+        }
+        #endregion
+
+        #region 天數判定
+        if (n == 0)
+        {
+            txtDay.text = "第 1 天";
+        }
+        else if (n == x * 2 && isMyTurn)
+        {
+            txtDay.text = "第 " + (x + 1) + " 天";
+        }
+        #endregion
+
+        //測試用
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            turn++;
         }
     }
     void OnBtnTurnEndClick()
